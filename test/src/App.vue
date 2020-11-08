40e8a9e2be96213f6d9904b8f909c0b4144d1e6d
gongjie <gongjie@otosaas.com>
2020-11-08 16:17:36 +0800

    tst

---
+++ test/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script>
+import { logger } from "../../lib";
+export default {
+  setup() {
+    logger().error('haha')
+  }
+}
+</script>
---
+++ test/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { init } from "../../lib";
+init()
+
+createApp(App).mount('#app')
